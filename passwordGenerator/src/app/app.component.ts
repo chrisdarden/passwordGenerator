841435d925e050a03149fe1e3f39d3eec676445a
Chris Darden <chris.a.darden@gmail.com>
2024-03-24 13:44:42 -0500

    password generator logic complete

--- passwordGenerator/src/app/app.component.ts
+++ passwordGenerator/src/app/app.component.ts
@@ -11,11 +11,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  length = 0;
   includeLetters: boolean = false;
   includeNumbers: boolean = false;
   includeSymbols: boolean = false;
-
   password: string = "";
+
+
+  onChangeLength = (value: string) => {
+    const parsedValue = parseInt(value); //
+
+    // checks to make sure that the value is a number
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 
   onChangeUseLetters = () => {
     this.includeLetters = !this.includeLetters;
@@ -33,14 +43,49 @@ export class AppComponent {
   }
 
   onButtonClick = () => {
+    const numbers = "1234567890"
+    const letters = "abcdefghijklmnopqrstuvwxyz"
+    const symbols = "!@#$%^&*()"
+
+    let validChars = "";
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+
+    // loop to generate password
+    let generatedPassword = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
     console.log(`
     About to generate a password with the following:
+    Length of the password: ${this.password.valueOf().length}
     Includes letters: ${this.includeLetters}
     Includes numbers: ${this.includeNumbers}
     Includes symbols: ${this.includeSymbols}
     `);
-    this.password = "password";
+
+
+
+
+
   }
+
+
+
+
+
 
 
 }
