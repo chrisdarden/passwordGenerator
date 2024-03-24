cc48ff7a539631185ebd2431ee6a9d9fa97391ac
Chris Darden <chris.a.darden@gmail.com>
2024-03-24 13:11:42 -0500

    added checkbox with generate methods

--- passwordGenerator/src/app/app.component.ts
+++ passwordGenerator/src/app/app.component.ts
@@ -11,9 +11,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
   password: string = "";
+
+  onChangeUseLetters = () => {
+    this.includeLetters = !this.includeLetters;
+    console.log(this.includeLetters);
+  }
+
+  onChangeUseNumbers = () => {
+    this.includeNumbers = !this.includeNumbers;
+    console.log(this.includeNumbers);
+  }
+
+  onChangeUseSymbols = () => {
+    this.includeSymbols = !this.includeSymbols;
+    console.log(this.includeSymbols);
+  }
+
   onButtonClick = () => {
+    console.log(`
+    About to generate a password with the following:
+    Includes letters: ${this.includeLetters}
+    Includes numbers: ${this.includeNumbers}
+    Includes symbols: ${this.includeSymbols}
+    `);
     this.password = "password";
   }
+
 
 }
